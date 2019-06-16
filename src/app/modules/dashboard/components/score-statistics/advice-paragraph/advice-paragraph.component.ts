6d3ebcfb49c74ff934c9b72aac0e09df07336b98
Grzegorz Kniazuk <grzegorz.kniazuk@gmail.com>
2019-06-16 21:25:00 +0200

    16.06 select on index refactor

--- src/app/modules/dashboard/components/score-statistics/advice-paragraph/advice-paragraph.component.ts
+++ src/app/modules/dashboard/components/score-statistics/advice-paragraph/advice-paragraph.component.ts
@@ -45,12 +45,11 @@ export class AdviceParagraphComponent {
 
     private get numberIndexButtonOptions(): SelectItem[] {
         return [
-            { label: 'Wszystkie', value: [ 0, 1, 2, 3, 4 ] },
-            { label: '1', value: [ 0 ] },
-            { label: '2', value: [ 1 ] },
-            { label: '3', value: [ 2 ] },
-            { label: '4', value: [ 3 ] },
-            { label: '5', value: [ 4 ] },
+            { label: '1', value: 0 },
+            { label: '2', value: 1 },
+            { label: '3', value: 2 },
+            { label: '4', value: 3 },
+            { label: '5', value: 4 },
         ];
     }
 
